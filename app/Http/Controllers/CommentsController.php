26a43538fd31aeba4d2eaeda82e1ae35c0d48a7a
macclinson <macclinson@gmail.com>
2017-11-13 00:34:04 +0100

    Fixed broken comment section

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -12,9 +12,10 @@ class CommentsController extends Controller
 {
     public function store(Post $post)
     {
-      $this->validate(request(), ['body' => 'required|min:10']);
-
       $post->addComment(request('body'));
+      // $this->validate(request(), ['body' => 'required|min:10']);
+      //
+      // $post->addComment(request('body'));
 
       return back();
     }
